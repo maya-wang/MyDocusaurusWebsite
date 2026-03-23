18e2d99095f832d1f8c24ada9880293e6aa2cbd6
maya-wang <maya_wang@163.com>
2026-03-23 16:55:44 +0800

    Test sidebar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,9 +46,8 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          path: 'docs/TestDocSiteGenerator',
-          sidebarPath: './sidebars.js',
-          // Please change this to your repo.
+             path: 'docs/TestDocSiteGenerator',
+  // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
