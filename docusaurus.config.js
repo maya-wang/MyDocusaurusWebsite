ec932bde3bf6173d44045d1a498d4a5062d09816
maya-wang <maya_wang@163.com>
2026-03-23 17:22:23 +0800

    Fix config error

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,8 +46,8 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-             path: 'docs/TestDocSiteGenerator',
-  // Please change this to your repo.
+          sidebarPath: './sidebars.js',
+          // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
